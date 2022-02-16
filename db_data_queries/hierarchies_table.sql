a8bad48d5a47c74050d7988f52cc5684c9d1ff34
ucancallmealicia <alicia.detelich@yale.edu>
2022-02-16 16:21:15 -0500

    delete old files

--- db_data_queries/hierarchies_table.sql
+++
@@ -1,7 +0,0 @@
-SELECT hierarchies.id
-	, hierarchies.root_record_id
-	, hierarchies.repo_id
-	, hierarchies.full_path
-	, hierarchies.lvl
-FROM hierarchies
-WHERE hierarchies.repo_id = 12
--- db_data_queries/restriction_table.sql
+++
@@ -1,8 +0,0 @@
-SELECT ao_id
-	, resource_note_text
-	, lvl
-	, path
-	, type_path
-	, end_path
-FROM accesshierarchies
-WHERE accesshierarchies.repo_id = 12
